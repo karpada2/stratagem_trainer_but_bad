f2c556289bfe4901dc3aa01433f2554ce2b4e5b6
karpada2 <gongrossman@gmail.com>
2024-12-19 12:54:30 +0200

    create global script of displayable stratagem names

---
+++ scripts/globals/stratagem_names.gd
@@ -0,0 +1,97 @@
+extends Node
+
+var sentries: Dictionary = {
+	"tesla tower" : "Tesla Tower",
+	"shield generator relay" : "Shield Generator Relay",
+	"rocket sentry" : "Rocket Sentry",
+	"mortar sentry" : "Mortar Sentry",
+	"machine gun sentry" : "Machine Gun Sentry",
+	"incendiary mines" : "Incendiary Mines",
+	"hmg emplacement" : "HMG Emplacement",
+	"gatling sentry" : "Gatling Sentry",
+	"flame sentry" : "Flame Sentry",
+	"ems mortar sentry" : "EMS Mortar Sentry",
+	"autocannon sentry" : "Autocannon Sentry",
+	"anti-tank mines" : "Anti-Tank Mines",
+	"anti-tank emplacement" : "Anti-Tank Emplacement",
+	"anti-personnel minefield" : "Anti-Personnel Minefield"
+}
+
+
+var support_weapons: Dictionary = {
+	"sterilizer" : "Sterilizer",
+	"stalwart" : "Stalwart",
+	"spear" : "Spear",
+	"recoilless rifle" : "Recoilless Rifle",
+	"railgun" : "Railgun",
+	"quasar cannon" : "Quasar Cannon",
+	"machine gun" : "Machine Gun",
+	"laser cannon" : "Laser Cannon",
+	"heavy machine gun" : "Heavy Machine Gun",
+	"grenade launcher" : "Grenade Launcher",
+	"flamethrower" : "Flamethrower",
+	"expendable anti-tank" : "Expendable Anti-Tank",
+	"commando" : "Commando",
+	"autocannon" : "Autocannon",
+	"arc thrower" : "Arc Thrower",
+	"anti-materiel rifle" : "Anti-Materiel Rifle",
+	"airburst rocket launcher" : "Airburst Rocket Launcher"
+}
+
+
+var backpacks: Dictionary = {
+	"supply pack" : "Supply Pack",
+	"shield generator pack" : "Shield Generator Pack",
+	"jump pack" : "Jump Pack",
+	"guard dog" : "\"Guard Dog\"",
+	"guard dog rover" : "\"Guard Dog\" Rover",
+	"guard dog dog breath" : "\"Guard Dog\" Dog Breath",
+	"directional shield" : "Directional Shield",
+	"ballistic shield backpack" : "Ballistic Shield Backpack"
+}
+
+
+var orbitals: Dictionary = {
+	"orbital walking barrage" : "Orbital Walking Barrage",
+	"orbital smoke strike" : "Orbital Smoke Strike",
+	"orbital railcannon strike" : "Orbital Railcannon Strike",
+	"orbital precision strike" : "Orbital Precision Strike",
+	"orbital napalm barrage" : "Orbital Napalm Barrage",
+	"orbital laser" : "Orbital Laser",
+	"orbital gatling barrage" : "Orbital Gatling Barrage",
+	"orbital gas strike" : "Orbital Gas Strike",
+	"orbital ems strike" : "Orbital EMS Strike",
+	"orbital airburst strike" : "Orbital Airburst Strike",
+	"orbital 380mm he barrage" : "Orbital 380mm HE Barrage",
+	"orbital 120mm he barrage" : "Orbital 120mm HE Barrage"
+}
+
+
+var eagles: Dictionary = {
+	"eagle strafing run" : "Eagle Strafing Run",
+	"eagle smoke strike" : "Eagle Smoke Strike",
+	"eagle napalm airstrike" : "Eagle Napalm Airstrike",
+	"eagle cluster bomb" : "Eagle Cluster Bomb",
+	"eagle airstrike" : "Eagle Airstrike",
+	"eagle 500kg bomb" : "Eagle 500KG Bomb",
+	"eagle 110mm rocket pods" : "Eagle 110mm Rocket Pods"
+}
+
+
+var exosuits: Dictionary = {
+	"patriot exosuit" : "Patriot Exosuit",
+	"emancipator exosuit" : "Emancipator Exosuit"
+}
+
+
+var misc: Dictionary = {
+	"upload data" : "Upload Data",
+	"sos beacon" : "SOS Beacon",
+	"seaf artillery" : "SEAF Artillery",
+	"resupply" : "Resupply",
+	"reinforce" : "Reinforce",
+	"orbital illumination flare" : "Orbital Illumination Flare",
+	"hellbomb" : "Hellbomb",
+	"eagle rearm" : "Eagle Rearm",
+	"sssd delivery" : "SSSD Delivery"
+}
